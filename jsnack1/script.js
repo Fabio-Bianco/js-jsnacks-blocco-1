53a8d2358fe922dca64c178dea931fe013d16d63
Fabio-Bianco <biancobfabio@gmail.com>
2025-02-27 12:30:44 +0100

    jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,14 @@
-console.log ('prova');
+
+
+
+let number= [];
+for (let i = 0; i < 2; i++) {
+    number[i] = parseFloat(prompt(`Inserisci il numero ${i + 1}:`));
+}
+if (number[0] > number[1]) {
+    console.log(`Il numero maggiore è: ${number[0]}`);
+} else if (number[1] > number[0]) {
+    console.log(`Il numero maggiore è: ${number[1]}`);
+} else {
+    console.log("I numeri sono uguali.");
+}
